19834a47872dc48ae0add89ee617054c75b4c283
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-03-14 15:35:14 +0200

    eka päivä pulkassa

---
+++ t1.js
@@ -0,0 +1,14 @@
+let fname="Teppo";
+
+console.log("Terve "+fname);
+
+let age=15;
+const lengt=182;
+
+console.log(fname+" on "+age+" vuotias");
+console.log(fname+" on "+lengt+" cm pitkä");
+
+age=16;
+//lengt=184; const muuttujan arvoa ei voi muokata
+console.log(fname+" on "+age+" vuotias");
+console.log(fname+" on "+lengt+" cm pitkä");
---
+++ t2.js
@@ -0,0 +1,12 @@
+for(let x=1; x<5; x++){
+    console.log("Rivi "+x);
+}
+//console.log("Lopussa x="+x);
+//let sanalla määritetty x on näkyvissä
+//vain for loopin sisällä
+
+for(var y=1; y<5; y++){
+    console.log("Rivi "+y);
+}
+console.log("Lopussa y="+y);
+//var sanalla määritetty y on globaalimuuttuja
